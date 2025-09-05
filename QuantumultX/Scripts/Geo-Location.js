8973bb5e781b57edfadafb8831468bc585294aa6
Centralmatrix3 <Centralmatrix3@users.noreply.github.com>
2025-09-05 23:50:45 +0800

    2025-09-05 23:50:45

---
+++ QuantumultX/Scripts/Geo-Location.js
@@ -0,0 +1,49 @@
+/*
+ * @name = Geo-Location
+ * @desc = Geo-Location
+ * @author = KOP-XIAO
+----------------------------------------
+[task_local]
+event-interaction https://raw.githubusercontent.com/Centralmatrix3/Matrix-io/master/QuantumultX/Scripts/Geo-Location.js, tag=节点信息查询, img-url=tag.circle.system, enabled=true
+----------------------------------------
+*/
+
+// var content= `<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: bold">` + response.body + `</p>`;
+
+  var url = "https://api.ip.sb/geoip"
+  var opts = {
+      policy: $environment.params
+  };
+  var myRequest = {
+      url: url,
+      opts: opts,
+      timeout: 4000
+  };
+ 
+  var message = ""
+  const paras = ["ip","isp","country_code","city"]
+  const paran = ["IP","ISP","地区","城市"]
+  $task.fetch(myRequest).then(response => {
+    message = response? json2info(response.body,paras) : ""
+      $done({"title": "    🔎 IP.SB 查询结果", "htmlMessage": message});
+  }, reason => {
+    message = "</br></br>🛑 查询超时"
+    message = `<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: bold;">` + message + `</p>`
+      $done({"title": "🔎 IP.SB 查询结果", "htmlMessage": message});
+  })
+
+
+function json2info(cnt,paras) {
+  var res = "------------------------------"
+  cnt =JSON.parse(cnt)
+  for (i=0;i<paras.length;i++) {
+    cnt[paras[i]] = paras[i] == "country_code"? cnt[paras[i]]+" ⟦"+flags.get(cnt[paras[i]].toUpperCase())+"⟧":cnt[paras[i]]
+    res = cnt[paras[i]]?   res +"</br><b>"+ "<font  color=>" +paran[i] + "</font> : " + "</b>"+ "<font  color=>"+cnt[paras[i]] +"</font></br>" : res
+  }
+  res =res+ "------------------------------"+"</br>"+"<font color=#6959CD>"+"<b>节点</b> ➟ " + $environment.params+ "</font>"
+  res =  `<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: thin">` + res + `</p>`
+  return res
+}
+
+
+var flags = new Map([[ "AC" , "🇦🇨" ] ,["AE","🇦🇪"], [ "AF" , "🇦🇫" ] , [ "AI" , "🇦🇮" ] , [ "AL" , "🇦🇱" ] , [ "AM" , "🇦🇲" ] , [ "AQ" , "🇦🇶" ] , [ "AR" , "🇦🇷" ] , [ "AS" , "🇦🇸" ] , [ "AT" , "🇦🇹" ] , [ "AU" , "🇦🇺" ] , [ "AW" , "🇦🇼" ] , [ "AX" , "🇦🇽" ] , [ "AZ" , "🇦🇿" ] , ["BA", "🇧🇦"], [ "BB" , "🇧🇧" ] , [ "BD" , "🇧🇩" ] , [ "BE" , "🇧🇪" ] , [ "BF" , "🇧🇫" ] , [ "BG" , "🇧🇬" ] , [ "BH" , "🇧🇭" ] , [ "BI" , "🇧🇮" ] , [ "BJ" , "🇧🇯" ] , [ "BM" , "🇧🇲" ] , [ "BN" , "🇧🇳" ] , [ "BO" , "🇧🇴" ] , [ "BR" , "🇧🇷" ] , [ "BS" , "🇧🇸" ] , [ "BT" , "🇧🇹" ] , [ "BV" , "🇧🇻" ] , [ "BW" , "🇧🇼" ] , [ "BY" , "🇧🇾" ] , [ "BZ" , "🇧🇿" ] , [ "CA" , "🇨🇦" ] , [ "CF" , "🇨🇫" ] , [ "CH" , "🇨🇭" ] , [ "CK" , "🇨🇰" ] , [ "CL" , "🇨🇱" ] , [ "CM" , "🇨🇲" ] , [ "CN" , "🇨🇳" ] , [ "CO" , "🇨🇴" ] , [ "CP" , "🇨🇵" ] , [ "CR" , "🇨🇷" ] , [ "CU" , "🇨🇺" ] , [ "CV" , "🇨🇻" ] , [ "CW" , "🇨🇼" ] , [ "CX" , "🇨🇽" ] , [ "CY" , "🇨🇾" ] , [ "CZ" , "🇨🇿" ] , [ "DE" , "🇩🇪" ] , [ "DG" , "🇩🇬" ] , [ "DJ" , "🇩🇯" ] , [ "DK" , "🇩🇰" ] , [ "DM" , "🇩🇲" ] , [ "DO" , "🇩🇴" ] , [ "DZ" , "🇩🇿" ] , [ "EA" , "🇪🇦" ] , [ "EC" , "🇪🇨" ] , [ "EE" , "🇪🇪" ] , [ "EG" , "🇪🇬" ] , [ "EH" , "🇪🇭" ] , [ "ER" , "🇪🇷" ] , [ "ES" , "🇪🇸" ] , [ "ET" , "🇪🇹" ] , [ "EU" , "🇪🇺" ] , [ "FI" , "🇫🇮" ] , [ "FJ" , "🇫🇯" ] , [ "FK" , "🇫🇰" ] , [ "FM" , "🇫🇲" ] , [ "FO" , "🇫🇴" ] , [ "FR" , "🇫🇷" ] , [ "GA" , "🇬🇦" ] , [ "GB" , "🇬🇧" ] , [ "HK" , "🇭🇰" ] ,["HU","🇭🇺"], [ "ID" , "🇮🇩" ] , [ "IE" , "🇮🇪" ] , [ "IL" , "🇮🇱" ] , [ "IM" , "🇮🇲" ] , [ "IN" , "🇮🇳" ] , [ "IS" , "🇮🇸" ] , [ "IT" , "🇮🇹" ] , [ "JP" , "🇯🇵" ] , [ "KR" , "🇰🇷" ] , [ "LU" , "🇱🇺" ] , [ "MO" , "🇲🇴" ] , [ "MX" , "🇲🇽" ] , [ "MY" , "🇲🇾" ] , [ "NL" , "🇳🇱" ] , [ "PH" , "🇵🇭" ] , [ "RO" , "🇷🇴" ] , [ "RS" , "🇷🇸" ] , [ "RU" , "🇷🇺" ] , [ "RW" , "🇷🇼" ] , [ "SA" , "🇸🇦" ] , [ "SB" , "🇸🇧" ] , [ "SC" , "🇸🇨" ] , [ "SD" , "🇸🇩" ] , [ "SE" , "🇸🇪" ] , [ "SG" , "🇸🇬" ] , [ "TH" , "🇹🇭" ] , [ "TN" , "🇹🇳" ] , [ "TO" , "🇹🇴" ] , [ "TR" , "🇹🇷" ] , [ "TV" , "🇹🇻" ] , [ "TW" , "🇨🇳" ] , [ "UK" , "🇬🇧" ] , [ "UM" , "🇺🇲" ] , [ "US" , "🇺🇸" ] , [ "UY" , "🇺🇾" ] , [ "UZ" , "🇺🇿" ] , [ "VA" , "🇻🇦" ] , [ "VE" , "🇻🇪" ] , [ "VG" , "🇻🇬" ] , [ "VI" , "🇻🇮" ] , [ "VN" , "🇻🇳" ] , [ "ZA" , "🇿🇦"]])
---
+++ Stash/Scripts/TestFlightX.js
@@ -0,0 +1,117 @@
+/*
+ * @name = TestFlightX
+ * @desc = TestFlightX
+ * @author = Skyxim
+----------------------------------------
+http:
+  script:
+    - match: ^https:\/\/testflight\.apple\.com((\/v3\/accounts\/.+?\/apps$)|(\/v2\/accounts\/.+?\/apps\/.+?\/builds\/.+))
+      name: TF强制安装
+      type: response
+      require-body: true
+      timeout: 120
+
+  mitm:
+    - "testflight.apple.com"
+
+script-providers:
+  TF强制安装:
+    url: https://raw.githubusercontent.com/Centralmatrix3/Matrix-io/master/Stash/Scripts/TestFlightX.js
+    interval: 86400
+----------------------------------------
+*/
+
+const url = $request.url
+ const responseBody = $response.body
+ 
+     ; (async () => {
+         const regeList = /.*?\/v3\/accounts\/.+?\/apps$/
+         const regeMainPage = /.*?\/v2\/accounts\/.+?\/apps\/.+?\/builds\/.+/
+         const regBiulds = /.*?\/v2\/accounts\/.+?\/apps\/.+?\/platforms\/ios\/trains\/.+?\/builds/
+         if (regeList.test(url)) {
+             $done({ body: list(responseBody) })
+             return
+         }
+ 
+         if (regeMainPage.test(url)) {
+             if (url.endsWith("install")) {
+                 console.log("is install")
+                 $done({ body: responseBody })
+                 return
+             }
+ 
+             $done({ body: info(responseBody) })
+             return
+         }
+ 
+         if (regBiulds.test(url)) {
+             $done({ body: builds(responseBody) })
+             return
+         }
+     })()
+         .catch(error => {
+             console.log(error)
+         }).finally(() => $done())
+ 
+ function info(responseBody) {
+     let body = JSON.parse(responseBody)
+     let family = {
+         "name": "Mac",
+         "unsupportedDevices": [
+         ],
+         "minimumSupportedDevice": null
+     }
+ 
+     for (const build of body.data.builds) {
+         build.platformCompatible = true
+         build.hardwareCompatible = true
+         build.compatible = true
+         build.permission = "install"
+ 
+ 
+         build.compatibilityData.compatibleDeviceFamilies.push(family)
+     }
+ 
+     let build = body.data.currentBuild
+     build.platformCompatible = true
+     build.hardwareCompatible = true
+     build.compatible = true
+     build.permission = "install"
+ 
+ 
+     build.compatibilityData.compatibleDeviceFamilies.push(family)
+     return JSON.stringify(body)
+ }
+ 
+ function list(responseBody) {
+     if (responseBody === "") {
+         $done({ body: responseBody })
+     }
+ 
+     let body = JSON.parse(responseBody)
+ 
+     for (const app of body.data) {
+         for (const p of app.platforms) {
+             if (p.name === "ios") {
+                 console.log(p.build.name)
+                 p.build.hardwareCompatible = true
+                 p.build.compatible = true
+             }
+         }
+     }
+ 
+     return JSON.stringify(body)
+ }
+ 
+ function builds(responseBody) {
+     let body = JSON.parse(responseBody)
+     if (body.error === null) {
+         for (const build of body.data) {
+             if (build.platform === "ios") {
+                 build.compatible = true
+             }
+         }
+     }
+ 
+     return JSON.stringify(body)
+ }
