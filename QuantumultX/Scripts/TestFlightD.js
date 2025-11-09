456a3cc40bc8fa14d63499a377715b9fadf4c412
Centralmatrix3 <Centralmatrix3@users.noreply.github.com>
2025-11-09 15:09:26 +0800

    2025-11-09 15:09:26

---
+++ QuantumultX/Scripts/TestFlightD.js
@@ -0,0 +1,16 @@
+/*
+ * @name = TestFlightD
+ * @desc = TestFlightD
+ * @author = NobyDa
+----------------------------------------
+[rewrite_local]
+^https?:\/\/testflight\.apple\.com\/v\d\/accounts\/.+?\/install$ url script-request-body https://raw.githubusercontent.com/Centralmatrix3/Matrix-io/master/QuantumultX/Scripts/TestFlightD.js
+
+[mitm]
+hostname = testflight.apple.com
+----------------------------------------
+*/
+
+let app = JSON.parse($request.body);
+app.storefrontId = '143441-19,29';
+$done({body:JSON.stringify(app)});
